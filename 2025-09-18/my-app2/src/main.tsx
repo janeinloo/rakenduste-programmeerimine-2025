3bbf10fc33a39780ac065afed4204cafb2eb1704
Jan Aaron Einloo <janeinlo@tlu.ee>
2025-09-19 20:45:05 +0300

    feat(ThemeProvider): add createTheme and ThemeProvider

--- 2025-09-18/my-app2/src/main.tsx
+++ 2025-09-18/my-app2/src/main.tsx
@@ -6,19 +6,33 @@ import Layout from "./App.tsx"
 import Home from "./Components/Home.tsx"
 import About from "./Components/About.tsx"
 import Something from "./Components/Something.tsx"
+import { createTheme, ThemeProvider } from "@mui/material"
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#333355"
+    },
+    secondary: {
+      main: "#ffffffff"
+    },
+  },
+})
 
 const root = document.getElementById("root") as HTMLElement;
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="something" element={<Something />} />
-        </Route>
-      </Routes>
-    </HashRouter>
+    <ThemeProvider theme={theme}>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="something" element={<Something />} />
+          </Route>
+        </Routes>
+      </HashRouter>
+    </ThemeProvider>
   </React.StrictMode>
-);
+)
