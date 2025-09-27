d8fbff2ad6432cbf4791f57cbc4bd351c15ab7cd
Jan Aaron Einloo <janeinlo@tlu.ee>
2025-09-28 00:52:25 +0300

    node modules remove

--- 2025-09-11/my-app/src/components/Counter.tsx
+++ 2025-09-11/my-app/src/components/Counter.tsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import "../App.css"
 
 function Counter() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState<number>(0)
 
   function increaseCounter(amount: number) {
     setCount(count => count + amount)
